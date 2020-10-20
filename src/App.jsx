8dfd75c6b207b5806ba0911d3a69cedb00bcb710
miri_jo <miri_jo@tmax.co.kr>
2020-10-20 17:35:39 +0900

    [feat] 프로젝트 설정

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.scss';
+
+const App = () => {
+    return (
+        <>
+            <header>Header</header>
+            <div id="content">
+                {/* TODO 라우터 설정 */}
+            </div>
+            <footer>Footer</footer>
+        </>
+    );
+};
+
+export default App;
