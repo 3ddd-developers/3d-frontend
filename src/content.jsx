390bd09d2e0abf59f4e4ed03bf08469737824a2c
dulcis-hortus <dulcishortus@gmail.com>
2020-10-21 12:56:02 +0900

    파일 수정 및 부트스트랩 라이브러리 제거

--- src/content.jsx
+++ src/content.jsx
@@ -1,27 +1,13 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
 
-const Content = () => {
-
+const WarpContent = ({children}) => {
     return (
-        <>
-            <div id="innerContent">
-            </div>
-        </>
+        <div id="innerContent">
+            {children}
+        </div>
     );
 }
 
 
 
-// const StudyOption = () => {
-//     return (
-//         <select>
-//             <option value="" selected disabled hidden>{}</option>       
-//             <option value={}>{}</option>       
-//             <option value={}>{}</option>       
-//         </select>
-//     );
-// }
-
-
-export default Content;
+export default WarpContent;
---
+++ src/pages/study/index.js
@@ -0,0 +1,4 @@
+export { default as StudyApply } from './StudyApply.jsx';
+export { default as StudyCreate } from './StudyCreate.jsx';
+export { default as StudyDetail } from './StudyDetail.jsx';
+
