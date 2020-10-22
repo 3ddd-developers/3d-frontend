21515b34da011ef356bc73adced8dfa7fe8ab5cc
dulcis-hortus <dulcishortus@gmail.com>
2020-10-22 14:49:30 +0900

    리액트 부트스트랩 설치

--- src/pages/study/StudyApply.jsx
+++ src/pages/study/StudyApply.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import {ButtonToolbar, Button} from 'react-bootstrap';
+
+
+
+
 
 const StudyApply = () => {
     return (
         <>
-            StudyApply
+            <h3>시범 코드</h3>
+            <ButtonToolbar>
+                <Button bsStyle="primary">primary</Button>
+            </ButtonToolbar>
+            <form>
+
+                <input></input>
+                <select>
+                    <option></option>
+                    <option></option>
+                    <option></option>
+                    <option></option>
+                    <option></option>
+                </select>
+            </form>
         </>
     );
 };
--- src/pages/study/StudyCreate.jsx
+++ src/pages/study/StudyCreate.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const StudyCreate = () => {
     return (
         <>
-            StudyCreate
+        <Container>
+            <Row>
+                <Col xs={4}>foo</Col>
+                <Col xs={{ span: 4, offset: 4 }}>bar</Col>
+            </Row>
+            <Row>
+                <Col xs={{ span: 3, offset: 3 }}>foo</Col>
+                <Col xs={{ span: 3, offset: 3 }}>bar</Col>
+            </Row>
+            <Row>
+                <Col xs={{ span: 6, offset: 3 }}>foo</Col>
+            </Row>
+        </Container>
         </>
     );
 };
