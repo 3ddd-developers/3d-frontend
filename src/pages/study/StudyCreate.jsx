7173b94fc79b4cdf12b8a3592378056b4c064309
mirijo <miri_jo@tmax.co.kr>
2020-10-23 21:50:16 +0900

    [feat] 스터디 생성 페이지 구현 및 로직 연동
    TODO: Create API 연동

--- src/pages/study/StudyCreate.jsx
+++ src/pages/study/StudyCreate.jsx
@@ -1,25 +1,81 @@
 import React from 'react';
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { Form, Button, Row, Col } from 'react-bootstrap';
+import { CgAsterisk } from "react-icons/cg";
+import { FaAsterisk } from "react-icons/fa";
+
 
 const StudyCreate = () => {
+
+    const handleSubmit = () => {
+        // TODO
+        // 1. validation
+        // 2. Create API 연동 
+    }
+
+    const onClickCancel = () => {
+        window.location.href = '/';
+    }
+
     return (
         <>
-        <Container>
-            <Row>
-                <Col xs={4}>foo</Col>
-                <Col xs={{ span: 4, offset: 4 }}>bar</Col>
+            <Row style={{ justifyContent: 'center' }}>
+                <h2 className="form-title">스터디 생성</h2>
             </Row>
-            <Row>
-                <Col xs={{ span: 3, offset: 3 }}>foo</Col>
-                <Col xs={{ span: 3, offset: 3 }}>bar</Col>
-            </Row>
-            <Row>
-                <Col xs={{ span: 6, offset: 3 }}>foo</Col>
-            </Row>
-        </Container>
+            <Row style={{ justifyContent: 'center' }}><p className="form-description">스터디를 생성하기 위한 정보를 입력해 주세요.</p></Row>
+
+            <hr className="form-hr" />
+            <Form style={{ width: '500px', margin: 'auto' }} onSubmit={handleSubmit} >
+
+                <Form.Group controlId="title">
+                    <Form.Label className="form-label">제목<CgAsterisk className="form-required" /></Form.Label>
+                    <Form.Control required placeholder="ex) 판교역 리액트 스터디 구해요!" />
+                </Form.Group>
+                <Form.Group controlId="title">
+                    <Form.Label className="form-label">주제<CgAsterisk className="form-required" /></Form.Label>
+                    <Form.Control required placeholder="ex) 리액트" />
+                </Form.Group>
+                <Row>
+                    <Col>
+                        <Form.Group controlId="region">
+                            <Form.Label className="form-label">지역<CgAsterisk className="form-required" /></Form.Label>
+                            <Form.Control required as="select">
+                                <option>서울</option>
+                                <option>경기</option>
+                                <option>인천</option>
+                                <option>제주</option>
+                                <option>강원</option>
+                            </Form.Control>
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group controlId="region">
+                            <Form.Label className="form-label">모집인원<CgAsterisk className="form-required" /></Form.Label>
+                            <Form.Control required as="select">
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </Form.Control>
+                        </Form.Group>
+                    </Col>
+                </Row>
+
+                <Form.Group controlId="description">
+                    <Form.Label className="form-label">내용</Form.Label>
+                    <Form.Control as="textarea" rows={3} />
+                </Form.Group>
+                <Row style={{ justifyContent: 'center', marginTop: '60px' }}>
+                    <Button type="submit" className="form-button" style={{ backgroundColor: 'pink', borderColor: 'pink' }}>
+                        생성
+  </Button>
+                    <Button variant="secondary" onClick={onClickCancel} style={{ marginLeft: '10px' }} className="form-button">
+                        취소
+  </Button>
+
+                </Row>
+
+            </Form>
         </>
     );
 };
