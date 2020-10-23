6cd1e3712e6a921a19ac68e435305314ef43604b
dulcis-hortus <dulcishortus@gmail.com>
2020-10-24 00:08:34 +0900

    Merge branch 'main' of https://github.com/3ddd-developers/3d-frontend into project/apply # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.
    사이드 프로젝트 생성 화면

--- src/pages/project/ProjectCreate.jsx
+++ src/pages/project/ProjectCreate.jsx
@@ -1,30 +1,41 @@
 import React from 'react';
-import { Form, FormControl, FormGroup } from "react-bootstrap/Container";
+import { Form, Button, Row, Col } from 'react-bootstrap';
+import { CgAsterisk } from "react-icons/cg";
+import { FaAsterisk } from "react-icons/fa";
 
-const Input = ({kind}) => {
-    return (
-        <>
-            <Form.Label htmlFor={kind}>{kind}</Form.Label>
-            <Form.Control type="text" placeholder={{kind} + "를 입력하세요" } id={kind} />
-        </>
-    );
-}
+// const Input = ({kind}) => {
+//     return (
+//         <>
+//             <Form.Label htmlFor={kind}>{kind}</Form.Label>
+//             <Form.Control type="text" placeholder={{kind} + "를 입력하세요" } id={kind} />
+//         </>
+//     );
+// }
 
 
-const Seletion = ({kind, option1, option2, option3}) => {
-    return (
-        <>
-            <label htmlFor={kind}>{kind}</label>
-            <select id={kind}>
-                <option>{option1}</option>
-                <option>{option2}</option>
-                <option>{option3}</option>
-            </select>
-        </>
-    );
-}
+// const Seletion = ({kind, option1, option2, option3}) => {
+//     return (
+//         <>
+//             <label htmlFor={kind}>{kind}</label>
+//             <select id={kind}>
+//                 <option>{option1}</option>
+//                 <option>{option2}</option>
+//                 <option>{option3}</option>
+//             </select>
+//         </>
+//     );
+// }
 
 const ProjectCreate = () => {
+    const handleSubmit = () => {
+        // TODO
+        // 1. validation
+        // 2. Create API 연동 
+    }
+
+    const onClickCancel = () => {
+        window.location.href = '/';
+    }
     return (
         <>
             {/* <Form action="url" method="post">
@@ -47,26 +58,66 @@ const ProjectCreate = () => {
             </Form>
             <button>CANCEL</button>
             <button type="submit">SUBMIT</button> */}
-            <Form>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-  </Form.Group>
+            <Row style={{ justifyContent: 'center' }}>
+                <h2 className="form-title">사이드 프로젝트 팀 생성</h2>
+            </Row>
+            <Row style={{ justifyContent: 'center' }}><p className="form-description">팀 생성을 위한 정보를 입력해 주세요.</p></Row>
+            <hr className="form-hr" />
 
-  <Form.Group controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Form.Group controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
+            <Form style={{ width: '500px', margin: 'auto' }} onSubmit={handleSubmit} >
+                        <Form.Group controlId="title">
+                            <Form.Label className="form-label">제목<CgAsterisk className="form-required" /></Form.Label>
+                            <Form.Control required placeholder="ex) '우리동네 맛집' 앱 개발 같이 해요!" />
+                        </Form.Group>
+                        <Form.Group controlId="region">
+                            <Form.Label className="form-label">지역<CgAsterisk className="form-required" /></Form.Label>
+                            <Form.Control required placeholder="ex) 서울 서초" />
+                        </Form.Group>
+                <Row>
+                <Col sm={4}>
+                        <Form.Group controlId="region">
+                            <Form.Label className="form-label">모임 방식<CgAsterisk className="form-required" /></Form.Label>
+                            <Form.Control required as="select">
+                                <option>온라인</option>
+                                <option>오프라인</option>
+                                <option>둘 다</option>
+                            </Form.Control>
+                        </Form.Group>
+                    </Col>
+                    <Col sm={4}>
+                        <Form.Group controlId="title">
+                            <Form.Label className="form-label">팀장<CgAsterisk className="form-required" /></Form.Label>
+                            <Form.Control required placeholder="ex) 김코딩" />
+                        </Form.Group>
+                    </Col>
+                    <Col sm={4}>
+                        <Form.Group controlId="region">
+                            <Form.Label className="form-label">모집인원<CgAsterisk className="form-required" /></Form.Label>
+                            <Form.Control required as="select">
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </Form.Control>
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Form.Group controlId="description">
+                    <Form.Label className="form-label">설명<CgAsterisk className="form-required" /></Form.Label>
+                    <Form.Control required as="textarea" style={{ resize : 'none'}} rows={3} placeholder="ex) 동네 맛집을 공유하는 어플입니다."/>
+                </Form.Group>
+                <Row style={{ justifyContent: 'center', marginTop: '60px' }}>
+                    <Button type="submit" className="form-button" style={{ backgroundColor: 'pink', borderColor: 'pink' }}>
+                        생성
   </Button>
-</Form>
+                    <Button variant="secondary" onClick={onClickCancel} style={{ marginLeft: '10px' }} className="form-button">
+                        취소
+  </Button>
+
+                </Row>
+
+            </Form>
         </>
     );
 };
