b879af9dd524af389f4d428dafdf376ece09302f
dulcis-hortus <dulcishortus@gmail.com>
2020-10-23 22:22:04 +0900

    프로젝트 생성(임시)

--- src/pages/project/ProjectApply.jsx
+++ src/pages/project/ProjectApply.jsx
@@ -3,31 +3,7 @@ import React from 'react';
 const ProjectApply = () => {
     return (
         <>
-            <h1>3D, platform for searching side project or study!</h1>
-            <form action="url" method="post">
-                <label htmlFor="제목">제목</label>
-                <input type="text" value="" id="제목" />
-                <label htmlFor="장소">장소</label>
-                <input type="text" id="장소" value="" />
-                <label htmlFor="진행 방식">진행 방식</label>
-                <select id="진행 방식">
-                    <option>오프라인</option>
-                    <option>온라인</option>
-                    <option>둘 다</option>
-                </select>
-                <label htmlFor="모집인원">모집 인원</label>
-                <select id="모집인원">
-                    <option>3명 이하</option>
-                    <option>3~8명</option>
-                    <option>8명 이상</option>
-                </select>
-                <label htmlFor="주제">주제</label>
-                <input type="text" id="주제" />
-                <label htmlFor="내용">내용</label>
-                <input type="text" id="내용" />
-            </form>
-            <button>CANCEL</button>
-            <button type="submit">SUBMIT</button>
+
         </>
     );
 };
--- src/pages/project/ProjectCreate.jsx
+++ src/pages/project/ProjectCreate.jsx
@@ -1,9 +1,72 @@
 import React from 'react';
+import { Form, FormControl, FormGroup } from "react-bootstrap/Container";
+
+const Input = ({kind}) => {
+    return (
+        <>
+            <Form.Label htmlFor={kind}>{kind}</Form.Label>
+            <Form.Control type="text" placeholder={{kind} + "를 입력하세요" } id={kind} />
+        </>
+    );
+}
+
+
+const Seletion = ({kind, option1, option2, option3}) => {
+    return (
+        <>
+            <label htmlFor={kind}>{kind}</label>
+            <select id={kind}>
+                <option>{option1}</option>
+                <option>{option2}</option>
+                <option>{option3}</option>
+            </select>
+        </>
+    );
+}
 
 const ProjectCreate = () => {
     return (
         <>
-            ProjectCreate
+            {/* <Form action="url" method="post">
+                <Input kind="제목" />
+                <Input kind="장소" />
+                <Seletion 
+                    kind="진행 방식"
+                    option1="오프라인"
+                    option2="온라인"
+                    option3="둘 다"
+                />
+                <Seletion 
+                    kind="모집 인원"
+                    option1="3명 이하"
+                    option2="3~8명"
+                    option3="8명 이상"
+                />
+                <Input kind="진행 방식" />
+                <Input kind="내용" />
+            </Form>
+            <button>CANCEL</button>
+            <button type="submit">SUBMIT</button> */}
+            <Form>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
         </>
     );
 };
