447f23045207f0e64e0fabf474d1ee09abbcb255
mirijo <miri_jo@tmax.co.kr>
2020-10-24 11:01:06 +0900

    [feat] validation 에 따른 css 적용 (error 처리) 및 api 연동 로직 추가

--- src/pages/study/StudyCreate.jsx
+++ src/pages/study/StudyCreate.jsx
@@ -1,17 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button, Row, Col } from 'react-bootstrap';
 import { CgAsterisk } from "react-icons/cg";
+import axios from 'axios';
+import classNames from 'classnames';
 
 
 const StudyCreate = () => {
+    const [title, setTitle] = useState('');
+    const [subject, setSubject] = useState('');
+    const [region, setRegion] = useState('서울');
+    const [number, setNumber] = useState(1);
+    const [content, setContent] = useState('');
+
+    const [error, setError] = useState({ title: false, content: false });
 
     const handleSubmit = () => {
         // TODO
         // Create API 연동 
+        if (title.length > 100) {
+            // TODO: 에러 메시지 노출
+            setError({ ...error, title: true });
+            return;
+        }
+
+        if (content.length > 500) {
+            // TODO: 에러 메시지 노출
+            setError({ ...error, content: true });
+            return;
+        }
+
+        // TODO: user_id (github) 연동 조사
+        let json = {
+            user_id: 123,
+            title: title,
+            content: content,
+            member_number: number,
+            place_seq: region,
+            subject_seq: subject
+        };
+
+        console.log(json);
+        return;
+        axios.post('/api/study/post', json)
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
 
     const onClickCancel = () => {
         window.location.href = '/';
+    }
+
+    const onChange = evt => {
+        switch (evt.target.id) {
+            case 'title':
+                setTitle(evt.target.value);
+                break;
+            case 'subject':
+                setSubject(evt.target.value);
+                break;
+            case 'region':
+                setRegion(evt.target.value);
+                break;
+            case 'number':
+                setNumber(evt.target.value);
+                break;
+            case 'content':
+                setContent(evt.target.value);
+                break;
+            default:
+                break;
+        }
+    }
+
+    const onFocus = evt => {
+        setError({ ...error, [evt.target.id]: false });
     }
 
     return (
@@ -26,17 +92,20 @@ const StudyCreate = () => {
 
                 <Form.Group controlId="title">
                     <Form.Label className="form-label">제목<CgAsterisk className="form-required" /></Form.Label>
-                    <Form.Control required placeholder="ex) 판교역 리액트 스터디 구해요!" />
+                    <Form.Control className={classNames({ 'form-error': error.title })} required aria-describedby="titleHelpBlock" placeholder="ex) 판교역 리액트 스터디 구해요!" onChange={onChange} onFocus={onFocus} />
+                    <Form.Text className={classNames({ 'form-text-error': error.title })} id="titleHelpBlock" muted>
+                        최대 100자 까지 입력할 수 있습니다.
+  </Form.Text>
                 </Form.Group>
-                <Form.Group controlId="title">
+                <Form.Group controlId="subject">
                     <Form.Label className="form-label">주제<CgAsterisk className="form-required" /></Form.Label>
-                    <Form.Control required placeholder="ex) 리액트" />
+                    <Form.Control required placeholder="ex) 리액트" onChange={onChange} />
                 </Form.Group>
                 <Row>
                     <Col>
                         <Form.Group controlId="region">
                             <Form.Label className="form-label">지역<CgAsterisk className="form-required" /></Form.Label>
-                            <Form.Control required as="select">
+                            <Form.Control required as="select" onChange={onChange}>
                                 <option>서울</option>
                                 <option>경기</option>
                                 <option>인천</option>
@@ -46,9 +115,9 @@ const StudyCreate = () => {
                         </Form.Group>
                     </Col>
                     <Col>
-                        <Form.Group controlId="region">
+                        <Form.Group controlId="number">
                             <Form.Label className="form-label">모집인원<CgAsterisk className="form-required" /></Form.Label>
-                            <Form.Control required as="select">
+                            <Form.Control required as="select" onChange={onChange}>
                                 <option>1</option>
                                 <option>2</option>
                                 <option>3</option>
@@ -59,9 +128,12 @@ const StudyCreate = () => {
                     </Col>
                 </Row>
 
-                <Form.Group controlId="description">
+                <Form.Group controlId="content">
                     <Form.Label className="form-label">내용</Form.Label>
-                    <Form.Control as="textarea" rows={3} />
+                    <Form.Control className={classNames({ 'form-error': error.content })} as="textarea" aria-describedby="contentHelpBlock" rows={3} onChange={onChange} onFocus={onFocus} />
+                    <Form.Text className={classNames({ 'text-text-error': error.content })} id="contentHelpBlock" muted>
+                        최대 500자 까지 입력할 수 있습니다.
+  </Form.Text>
                 </Form.Group>
                 <Row style={{ justifyContent: 'center', marginTop: '60px' }}>
                     <Button type="submit" className="form-button" style={{ backgroundColor: '#80AAA6', borderColor: '#80AAA6' }}>
