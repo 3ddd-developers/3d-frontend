8d7cf7e996b8a6c41a8b420ef662db9011064229
mirijo <miri_jo@tmax.co.kr>
2020-10-24 14:20:42 +0900

    [feat] 스터디 지원하기 모달창 구현

--- src/pages/study/StudyDetail.jsx
+++ src/pages/study/StudyDetail.jsx
@@ -9,7 +9,11 @@ const StudyDetail = () => {
     const [status, setStatus] = useState('모집중');
 
     const handleClose = () => setShow(false);
-    const handleApply = () => setShow(true);
+    const handleShow = () => setShow(true);
+
+    const handleApply = () => {
+
+    }
 
     const goMain = () => {
         window.location.href = '/';
@@ -60,7 +64,7 @@ const StudyDetail = () => {
 
                 </Form.Group>
                 <Row style={{ justifyContent: 'center', marginTop: '60px' }}>
-                    {status === '모집중' && <Button onClick={handleApply} className="form-button" style={{ backgroundColor: '#80AAA6', borderColor: '#80AAA6' }}>
+                    {status === '모집중' && <Button onClick={handleShow} className="form-button" style={{ backgroundColor: '#80AAA6', borderColor: '#80AAA6' }}>
                         지원하기
   </Button>}
                     <Button onClick={goMain} className="form-button" style={{ backgroundColor: '#D7CDC2', borderColor: '#D7CDC2', marginLeft: '10px' }}>
@@ -72,14 +76,19 @@ const StudyDetail = () => {
             </Form>
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
-                    <Modal.Title>스터디 지원</Modal.Title>
+                    <Modal.Title className='modal-title'>스터디 지원</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>[앵귤러 스터디 모집해요] 스터디 지원에 </Modal.Body>
+                <Modal.Body style={{ fontSize: '13px' }}>[앵귤러 스터디 모집해요] 스터디 관리자에게 전송할 메시지를 입력해 주세요.
+                <Form.Group controlId="content" style={{ marginTop: '10px' }}>
+                        <Form.Control as="textarea" aria-describedby="contentHelpBlock" rows={3} />
+
+                    </Form.Group>
+                </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
+                    <Button className="form-button" style={{ width: '100px', backgroundColor: '#D7CDC2', borderColor: '#D7CDC2', marginLeft: '10px' }} onClick={handleClose}>
                         취소
           </Button>
-                    <Button variant="primary" onClick={handleClose}>
+                    <Button className="form-button" style={{ width: '100px', backgroundColor: '#80AAA6', borderColor: '#80AAA6' }} onClick={handleApply}>
                         지원하기
           </Button>
                 </Modal.Footer>
