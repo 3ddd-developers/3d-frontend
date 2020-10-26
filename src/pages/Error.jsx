6bb693436032f2d8c0fdf25a6e37b18b7b094ac6
mirijo <miri_jo@tmax.co.kr>
2020-10-26 11:08:04 +0900

    [feat] 에러 페이지에 뒤로가기, 메인으로 이동 버튼 추가 및 로직 구현

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Button } from 'react-bootstrap';
 import { BiError } from "react-icons/bi";
 
 
 const Error = () => {
+    const goBack = () => {
+        // console.log(window.history);
+        window.history.back();
+    }
+
+    const goMain = () => {
+        window.location.href = '/';
+    }
+
     return (
         <Row style={{ justifyContent: 'center' }}>
             <Col>
@@ -15,6 +24,15 @@ const Error = () => {
                 </Row>
                 <Row style={{ justifyContent: 'center' }}>
                     <span style={{ color: '#80AAA6', fontWeight: 'bold', fontSize: '20px' }}>찾을 수 없는 페이지입니다.</span>
+                </Row>
+                <Row style={{ justifyContent: 'center', marginTop: '30px' }}>
+                    <Button onClick={goBack} className="form-button" style={{ backgroundColor: '#80AAA6', borderColor: '#80AAA6' }}>
+                        뒤로가기
+  </Button>
+                    <Button onClick={goMain} className="form-button" style={{ backgroundColor: '#D7CDC2', borderColor: '#D7CDC2', marginLeft: '10px' }}>
+                        메인으로
+  </Button>
+
                 </Row>
             </Col>
 
