7feb7bda914a67b1a3cb9d60a667b9aa442e90ba
dulcis-hortus <dulcishortus@gmail.com>
2020-10-29 22:57:22 +0900

    깃헙 로그인 기능

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ const config = {
     study: `${document.location.origin}/api/study`,
     github: {
         url: `${document.location.origin}/github`,
-        client_id: '',
-        client_secret: ''
+        client_id: '2743e8896c72052939a2',
+        client_secret: 'a0eb8c007aa8ec7c17cdf3a94a4542a92fcdf36a'
     },
     github_api: `${document.location.origin}/api/github`,
 };
