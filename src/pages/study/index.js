9eb281d814e42515da233df3419d682232871249
mirijo <miri_jo@tmax.co.kr>
2020-10-30 11:42:07 +0900

    [feat] 스터디 관련 상태 코드 상수 파일로 관리

--- src/pages/study/index.js
+++ src/pages/study/index.js
@@ -1,4 +1,5 @@
 export { default as StudyApply } from './StudyApply.jsx';
 export { default as StudyCreate } from './StudyCreate.jsx';
 export { default as StudyDetail } from './StudyDetail.jsx';
+export { SPACE_CODE, STATUS_CODE } from './const.js';
 
